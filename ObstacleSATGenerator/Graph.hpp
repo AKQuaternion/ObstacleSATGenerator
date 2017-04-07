48f4596f056586c9b3bc49d73a5488e69824e6a0
Chris Hartman <chris.m.hartman@gmail.com>
2017-04-07 13:45:30 -0800

    Renamed Graph.h to Graph.hpp

---
+++ ObstacleSATGenerator/Graph.hpp
@@ -0,0 +1,42 @@
+/*
+ * Graph.h
+ *
+ *  Created on: Apr 23, 2012
+ *      Author: hartman
+ */
+
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+typedef size_t Vertex;
+typedef std::vector<Vertex> Path;
+typedef std::vector<Path > PathGroup;
+
+class Graph
+{
+    friend std::ostream & operator <<(std::ostream & os, const Graph & g);
+    
+public:
+    
+    Graph(const std::string &fname);
+    size_t numVerts() const;
+    PathGroup allPathsBetweenNonAdjacentVertices();
+    bool edge(Vertex i, Vertex j);
+    
+private:
+    bool notAdjacentToAnyButLast(const Path &p, Vertex vert) const;
+    PathGroup allPathsFromVOfLengthIOrLess(Vertex v, size_t i);
+    void filterPathsIncreasing(PathGroup &paths);
+    
+    std::vector<std::vector<int> > _adjacencies;
+};
+
+std::ostream & operator<<(std::ostream &, const Graph & g);
+void printPathGroup(const PathGroup &pl);
+
+#endif /* GRAPH_H_ */
