b47628852f3cd00932395c2335e9eb26910ea5f2
Chris Hartman <chris.m.hartman@gmail.com>
2017-04-07 10:32:00 -0800

    Project created.

--- ObstacleSATGenerator/main.cpp
+++ ObstacleSATGenerator/main.cpp
@@ -7,9 +7,10 @@
 //
 
 #include <iostream>
+using std::cout;
+using std::endl;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+
     return 0;
 }
