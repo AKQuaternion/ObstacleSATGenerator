ac717398313eb8832a186cf53a8107fc20ec228d
Chris Hartman <chris.m.hartman@gmail.com>
2017-04-07 13:44:21 -0800

    Added SATInstanceGenerator files

---
+++ ObstacleSATGenerator/SATInstanceGenerator.cpp
@@ -0,0 +1,9 @@
+//
+//  SATInstanceGenerator.cpp
+//  ObstacleSATGenerator
+//
+//  Created by Chris Hartman on 4/7/17.
+//  Copyright © 2017 Chris Hartman. All rights reserved.
+//
+
+#include "SATInstanceGenerator.hpp"
---
+++ ObstacleSATGenerator/SATInstanceGenerator.hpp
@@ -0,0 +1,16 @@
+//
+//  SATInstanceGenerator.hpp
+//  ObstacleSATGenerator
+//
+//  Created by Chris Hartman on 4/7/17.
+//  Copyright © 2017 Chris Hartman. All rights reserved.
+//
+
+#ifndef SATInstanceGenerator_hpp
+#define SATInstanceGenerator_hpp
+
+class SATInstanceGenerator {
+public:
+private:
+};
+#endif /* SATInstanceGenerator_hpp */
