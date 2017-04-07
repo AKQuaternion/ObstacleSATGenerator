528c01524c30349c0445d363107268fa48601189
Chris Hartman <chris.m.hartman@gmail.com>
2017-04-07 10:28:46 -0800

    Initial Commit

---
+++ ObstacleSATGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ObstacleSATGenerator
+//
+//  Created by Chris Hartman on 4/7/17.
+//  Copyright © 2017 Chris Hartman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
