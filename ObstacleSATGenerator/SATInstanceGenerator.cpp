998c06a0559a93db0f519569e9ebea339ad51114
Chris Hartman <chris.m.hartman@gmail.com>
2017-04-08 09:16:41 -0800

    Five point rule clauses sourced at aa can't have aa in the other four vertices.

--- ObstacleSATGenerator/SATInstanceGenerator.cpp
+++ ObstacleSATGenerator/SATInstanceGenerator.cpp
@@ -96,9 +96,13 @@ void SATInstanceGenerator::addFivePointRuleClauses() {
     for(Vertex aa=0;aa<numVertices();++aa)
         for(Vertex bb=0;bb<numVertices();++bb) {
             if(aa==bb) continue;
-            for(Vertex cc=bb+1;cc<numVertices();++cc)
-                for(Vertex dd=cc+1;dd<numVertices();++dd)
+            for(Vertex cc=bb+1;cc<numVertices();++cc) {
+                if(aa==cc) continue;
+                for(Vertex dd=cc+1;dd<numVertices();++dd) {
+                    if(aa==dd) continue;
                     for(Vertex ee=dd+1;ee<numVertices();++ee) {
+                        if(aa==ee) continue;
+                        
                         auto abc = variableForTriangle(aa,bb,cc);
                         auto abd = variableForTriangle(aa,bb,dd);
                         auto abe = variableForTriangle(aa,bb,ee);
@@ -123,6 +127,8 @@ void SATInstanceGenerator::addFivePointRuleClauses() {
                         _sat.addClause({-abc,-abd,-abe, acd,-ace, ade});
                         _sat.addClause({-abc,-abd,-abe,-acd, ace,-ade});
                     }
+                }
+            }
         }
 }
 
