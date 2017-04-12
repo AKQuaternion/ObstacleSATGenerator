a8630485614fa977a5d73638ad56bac159f7238f
Chris Hartman <chris.m.hartman@gmail.com>
2017-04-12 13:25:54 -0800

    Fix return type deduction

--- ObstacleSATGenerator/RangeUtilities.hpp
+++ ObstacleSATGenerator/RangeUtilities.hpp
@@ -27,16 +27,16 @@ private:
 
 template <class Iter>
 Range<Iter> makeRange(Iter begin, Iter end) {
-    return {begin, end};
+    return Range<Iter>(begin, end);
 }
 
 template <typename T>
-auto interior(T &t) {
+auto interior(T &t) -> Range<decltype(t.begin())> {
     auto newBegin {t.begin()};
     ++newBegin;
     auto newEnd {t.end()};
     --newEnd;
-    return makeRange(newBegin,newEnd);
+    return {newBegin,newEnd};
 }
 
 template <typename T>
